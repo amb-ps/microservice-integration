964f84ad567117ce858efc3e46819ec114f14725
Amith M B <amith.mb@publicissapient.com>
2024-10-23 23:20:41 +0530

    refactored working code

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,27 +1,34 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ProductResponse;
-import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 @RestController
 public class ProductController {
 
-    private final ProductService productService;
+    private final WebClient webClient;
 
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
+    @Autowired
+    public ProductController(WebClient webClient) {
+        this.webClient = webClient;
     }
-
 
     @GetMapping("/products")
     public List<String> findAllProducts() {
-        return productService.getProuctListFromMicro();
+        String productApiUrl = "http://eca.aws.service.demo.eca.aws.service.demo:8080/products";
+
+        return webClient.get()
+                .uri(productApiUrl)
+                .retrieve()
+                .bodyToFlux(String.class)
+                .collectList()
+                .block();
     }
 
     @GetMapping("/integrated/products")
